92ca98082bc078e11fef8ab9990682997c4d8817
unknown <mailmeakilesh997@gmail.com>
2024-01-12 19:59:55 +0530

    live api and Shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import RestaurantCardShimmer from "./RestaurantCardShimmer";
 import { restaurantsList } from "../utils/mockData";
 
 const Body = () => {
   //Here we have created the state variable of Restaurants list using mock data
   //And Notice we have created one attribute name as key this is important to create a key attribute oterwise react will give warning .
   //And we have created the logic to filter the component having rating more then 4 onclick of button
-  const [listOfRestaurants, setListOfRestaurants] = useState(
-    restaurantsList.data.restaurants
-  );
-  return (
+
+  //This below local state variable is used to read the data from local and keep in mind we are using "optional chaining"
+  //   const [listOfRestaurants, setListOfRestaurants] = useState(
+  //     restaurantsList?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+  //       .restaurants
+  //   );
+
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  const [filteredList, setFilteredList] = useState(listOfRestaurants);
+
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.474679&lng=77.1048978&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const restaurantsList = await response.json();
+    console.log(
+      restaurantsList?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        .restaurants
+    );
+    setListOfRestaurants(
+      restaurantsList?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        .restaurants
+    );
+    setFilteredList(
+      restaurantsList?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        .restaurants
+    );
+  };
+
+  //Below we shows the conditional renderring based on the data available or not and till the data isnt availble we are showing the "Shimmer UI" it creates the illusion to the user that site is loading
+
+  return listOfRestaurants.length === 0 ? (
+    <RestaurantCardShimmer />
+  ) : (
     <div className="app-body">
-      <div className="search">Search</div>
+      <div className="search-container">
+        <input
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        ></input>
+        <button
+          onClick={() => {
+            setFilteredList(
+              listOfRestaurants.filter((restaurant) =>
+                restaurant.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              )
+            );
+          }}
+        >
+          Search
+        </button>
+      </div>
       {/* This code will filter the card according to the rating greater thehn 4.3
       Writter below code shows the way to write the code same code in short way */}
       {/* <button
@@ -36,7 +92,7 @@ const Body = () => {
         Click ME to Filter Greater 4.3
       </button>
       <div className="restaurant-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredList.map((restaurant) => (
           <RestaurantCard
             key={restaurant.info.id}
             restaurantData={restaurant}
---
+++ src/components/RestaurantCardShimmer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const RestaurantCardShimmer = () => {
+  return (
+    <div className="restaurant-card-shimmer-container">
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+      <div className="restaurant-card-shimmer"></div>
+    </div>
+  );
+};
+
+export default RestaurantCardShimmer;
